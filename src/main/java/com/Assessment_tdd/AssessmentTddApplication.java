9d516c57529c64b2eda8390e75d2ddde39d89b09
Samiksha86 <samiksha.lohe6@gmail.com>
2025-01-17 17:55:24 +0530

    initial commit

---
+++ src/main/java/com/Assessment_tdd/AssessmentTddApplication.java
@@ -0,0 +1,13 @@
+package com.Assessment_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentTddApplication.class, args);
+	}
+
+}

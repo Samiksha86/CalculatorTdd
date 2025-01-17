bebc9b3bed03fc8eb42568c576b12f466765eafc
Samiksha86 <samiksha.lohe6@gmail.com>
2025-01-17 18:09:31 +0530

    test case with one input passed

--- src/main/java/com/Assessment_tdd/Calculator.java
+++ src/main/java/com/Assessment_tdd/Calculator.java
@@ -3,6 +3,10 @@ package com.Assessment_tdd;
 public class Calculator {
 
     public static int add(String string){
+        if(!string.isEmpty()) {
+            return Integer.parseInt(string);
+        }
+
         return  0;
     }
 }
